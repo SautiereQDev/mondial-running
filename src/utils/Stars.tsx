5fda97d9b1b027ceda2e84a79b13fb97e31d3812
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2025-02-28 16:40:50 +0100

    refactor Stars component to round ratings to the nearest 0.5 and improve star rendering logic

--- src/utils/Stars.tsx
+++ src/utils/Stars.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import {Star, StarHalf} from "lucide-react";
 
 export default function Stars({rating}: Readonly<{ rating: number }>) {
+	// Arrondir au multiple de 0.5 le plus proche
+	const roundedRating = Math.round(rating * 2) / 2;
+	const fullStars = Math.floor(roundedRating);
+	const hasHalfStar = roundedRating % 1 !== 0;
+
 	return (
-		<div className="flex">{Array(Math.floor(rating)).fill(String(0)).map((_, index) => (
-			<Star key={index} fill="#FFC633" stroke="none"/>
-		))
-		}
-			{rating % 1 !== 0 && <StarHalf fill="#FFC633" stroke="none"/>}
+		<div className="flex">
+			{Array(fullStars).fill(0).map((_, index) => (
+				<Star key={index} fill="#FFC633" stroke="none"/>
+			))}
+			{hasHalfStar && <StarHalf fill="#FFC633" stroke="none"/>}
 		</div>
 	)
 }
